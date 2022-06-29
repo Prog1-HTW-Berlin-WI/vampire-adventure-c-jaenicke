19884a780ed2ec32587273b0e7dce0c957d668ef
c-jaenicke <c-jaenicke@pm.me>
2022-06-29 15:17:58 +0200

    menu improvements, consistent elements, texts, controlls

--- app/VampireAdventureApp.java
+++ app/VampireAdventureApp.java
@@ -26,10 +26,10 @@ public class VampireAdventureApp {
         while (true) {
             showMenu();
             // int choice = readUserInput();
-            System.out.print("Enter a number between 1 and 6:\t");
+            System.out.print("\nEnter a number between 1 and 6:\t");
             int choice = readIntInput();
             handle(choice);
-            System.out.println("====================");
+            System.out.println("\n========================================");
         }
     }
 
@@ -83,7 +83,6 @@ public class VampireAdventureApp {
                 showMenu();
             }
                 break;
-
         }
     }
 
@@ -93,15 +92,16 @@ public class VampireAdventureApp {
     private static void showMenu() {
 
         String menuItems[] = { "",
-                "(1)\t Create Vampire",
-                "(2)\t Show Selected Vampire",
+                "(1)\t Create Vampires",
+                "(2)\t Show Statistics of Vampires (Show Selected Vampire)",
                 "(3)\t List all Vampires",
-                "(4)\t Delete Vampire",
-                "(5)\t Start nightly adventure",
+                "(4)\t Delete Minion Vampires",
+                "(5)\t Start Nightly Adventure",
                 "(6)\t Quit",
         };
 
-        System.out.println("\nVampire Adventures 1.1\n");
+        System.out.println("========================================");
+        System.out.println("\nVampire Adventures 1.0\n");
         for (int i = 1; i < menuItems.length; i++) {
             System.out.println(menuItems[i]);
         }
@@ -114,30 +114,31 @@ public class VampireAdventureApp {
      * hitting 0 goes back to main menu
      */
     private static void showVampires() {
-        System.out.println("====================");
-        System.out.println("\nAll vampires of your group\n");
-
-        Vampire[] members = playerParty.getMembers();
-
-        if (playerParty.getCreator() != null) {
-            System.out.println("(1)\tCreator Vampire");
-        } else {
-            System.out.println("No creator vampire available! Create one in the main menu using (1)");
-        }
-
-        listMembers(2);
-
         while (true) {
+            System.out.println("\n========================================");
+            System.out.println("\nAll vampires of your group\n");
+
+            Vampire[] members = playerParty.getMembers();
+
+            if (playerParty.getCreator() != null) {
+                System.out.println("(1)\t" + playerParty.getCreator().getName());
+            } else {
+                System.out.println("No creator vampire available! Create one in the main menu!\n");
+            }
+
+            listMembers(2);
+
             System.out.print("\nChoose a vampire to show the stats of or (0) to go to the main menu: ");
 
             int input = readIntInput();
-
             if (input == 0) {
                 // go back to main menu
                 return;
             } else if (input == 1) {
                 showVampireStats(playerParty.getCreator());
                 break;
+            } else if (members.length + 2 < input) {
+                System.out.println("That is not a valid vampire!");
             } else if (members[input - 2] == null) {
                 System.out.println("That is not a valid vampire!");
             } else {
@@ -152,20 +153,18 @@ public class VampireAdventureApp {
      * @param vamp
      */
     public static void showVampireStats(Vampire vamp) {
-        System.out.println("====================");
+        System.out.println("\n========================================");
         System.out.println("\nVampire Stats\n");
 
-        System.out.println("You have selected " + vamp.getName());
+        System.out.println("You have selected " + vamp.getName() + "\n");
 
         System.out.println("Name: " + vamp.getName());
         System.out.println("Age: " + vamp.getAge());
         // TODO show more attributes here
 
-        System.out.println("Press 0 to go back to the main menu!");
-
+        System.out.print("\nEnter 0 to go back to the menu: ");
         while (true) {
             int input = readIntInput();
-
             if (input == 0) {
                 // go back to main menu
                 return;
@@ -179,26 +178,26 @@ public class VampireAdventureApp {
      * delete a vampire
      */
     private static void deleteVampire() {
-        System.out.println("====================");
-        System.out.println("\nDelete a vampire\n");
-
-        Vampire[] members = playerParty.getMembers();
-
-        listMembers(1);
-
         while (true) {
+            System.out.println("\n========================================");
+            System.out.println("\nDelete a vampire\n");
+
+            Vampire[] members = playerParty.getMembers();
+
+            listMembers(1);
+
             System.out.print("\nChoose a vampire to delete or (0) to go to the main menu: ");
 
             int input = readIntInput();
-
             if (input == 0) {
                 // go back to main menu
                 return;
+            } else if (members.length + 2 < input) {
+                System.out.println("That is not a valid vampire!");
             } else if (members[input - 1] == null) {
                 System.out.println("That vampire doesn't exist!");
             } else {
                 playerParty.deleteMember(input - 1);
-                return;
             }
         }
 
@@ -208,46 +207,41 @@ public class VampireAdventureApp {
      * show list of all party members
      */
     private static void listAllVampires() {
-        System.out.println("====================");
+        System.out.println("\n========================================");
         System.out.println("\nAll vampires of your group\n");
 
-        Vampire[] members = playerParty.getMembers();
-
-        System.out.println("The creator vampire is:");
-        System.out.println(playerParty.getCreator());
-        System.out.println("\n");
-
-        int emptyCounter = 0;
-        for (int i = 0; i < members.length; i++) {
-            if (members[i] == null) {
-                emptyCounter = emptyCounter + 1;
-                if (emptyCounter == members.length) {
-                    System.out.println("Looks like you don't have any members or they all have died!");
-                }
-            } else {
-                System.out.println("Member " + (i + 1) + "\t" + members[i]);
-            }
-
+        if (playerParty.getCreator() != null) {
+            System.out.println("The creator vampire is:\n\t" + playerParty.getCreator().getName() + "\n");
+        } else {
+            System.out.println("No creator vampire exists! Create one in the main menu!\n");
         }
 
-        // TODO wait until key press to go back
-        waitSeconds(3);
+        System.out.println("Your minions are:");
+        listMembers(1);
 
+        // wait for user to press 0 to go back to main menu
+        System.out.print("\nEnter 0 to go back to the menu: ");
+        while (true) {
+            int input = readIntInput();
+            if (input == 0) {
+                return;
+            }
+        }
     }
 
     /**
      * create start vampires
      */
     private static void createVampire() {
-        System.out.println("====================");
+        System.out.println("\n========================================");
         System.out.println("\nVampire Creation Menu\n");
 
         // create creator vampire
         System.out.println("Create your creator vampire");
-        System.out.print("What is the name of the vampire: ");
+        System.out.print("\tWhat is the name of the vampire: ");
         String name = readStringInput();
 
-        System.out.print("How old is the vampire: ");
+        System.out.print("\tHow old is the vampire: ");
         int age = readIntInput();
 
         CreatorVampire cVampire = new CreatorVampire(name, age);
@@ -256,10 +250,10 @@ public class VampireAdventureApp {
         // create first minion
         System.out.println("\nCreate your first minion vampire");
 
-        System.out.print("What is the name of the vampire: ");
+        System.out.print("\tWhat is the name of the vampire: ");
         name = readStringInput();
 
-        System.out.print("How old is the vampire: ");
+        System.out.print("\tHow old is the vampire: ");
         age = readIntInput();
 
         Vampire mVamp1 = new Vampire(name, age);
@@ -269,47 +263,58 @@ public class VampireAdventureApp {
         // create second minion
         System.out.println("\nCreate your second minion vampire");
 
-        System.out.print("What is the name of this vampire: ");
+        System.out.print("\tWhat is the name of this vampire: ");
         name = readStringInput();
 
-        System.out.print("How old is this vampire: ");
+        System.out.print("\tHow old is this vampire: ");
         age = readIntInput();
 
         Vampire mVamp2 = new Vampire(name, age);
         mVamp2.setCreator(cVampire);
         playerParty.addMember(mVamp2);
 
-        // TODO wait until key press to go back
-        waitSeconds(3);
+        // wait for user to press 0 to go back to main menu
+        System.out.print("\nEnter 0 to go back to the menu: ");
+        while (true) {
+            int input = readIntInput();
+            if (input == 0) {
+                return;
+            }
+        }
     }
 
     /**
      * Create predefined Vampires for testing purposes
+     * "Create Vampire" needs to be called first
      */
     private static void debugCreateVampire() {
-        System.out.println("====================");
-        System.out.println("\nDEBUG\tVampire Creation Menu\n");
+        System.out.println("\n========================================");
+        System.out.println("\n!!--- DEBUG Vampire Creation Menu\n");
 
-        System.out.println("\nCreating creator vampire");
-        System.out.println("Name: Name Creator Vampire, Age: 300");
-        CreatorVampire cVampire = new CreatorVampire("Name Creator Vampire", 300);
+        // TODO when the vampire class is expanded, fill out more of the attributes
+
+        System.out.println("Creating creator vampire\nName: The Creator Vampire\n Age: 300");
+        CreatorVampire cVampire = new CreatorVampire("The Creator Vampire", 300);
         playerParty.setCreator(cVampire);
 
-        System.out.println("\nCreating first minion vampire");
-        System.out.println("Name: Someone Minion, Age: 100");
-        Vampire mVamp1 = new Vampire("Someone Minion", 100);
+        System.out.println("\nCreating first minion vampire\nName: First Minion Vampire\n Age: 100");
+        Vampire mVamp1 = new Vampire("First Minion Vampire", 100);
         mVamp1.setCreator(cVampire);
         playerParty.addMember(mVamp1);
 
-        System.out.println("\nCreating second minion vampire");
-        System.out.println("Name: Someone Second Minion, Age: 200");
-        Vampire mVamp2 = new Vampire("Someone Second Minion", 200);
+        System.out.println("\nCreating second minion vampire\nName: Second Minion Vampire\n Age: 200");
+        Vampire mVamp2 = new Vampire("Second Minion Vampire", 200);
         mVamp2.setCreator(cVampire);
         playerParty.addMember(mVamp2);
 
-        System.out.println("\nGoing back to main menu in 3 seconds!");
-        waitSeconds(3);
-
+        // wait for user to press 0 to go back to main menu
+        System.out.print("\nEnter 0 to go back to the menu: ");
+        while (true) {
+            int input = readIntInput();
+            if (input == 0) {
+                return;
+            }
+        }
     }
 
     /**
@@ -334,7 +339,8 @@ public class VampireAdventureApp {
      */
     private static int readIntInput() {
         while (!scanner.hasNextInt()) {
-            System.out.println("No number or input detected! Please try again...");
+            // System.out.println("No number or input detected! Please try again...");
+            System.out.println("That is not a valid input! Please try again...");
             scanner.next();
         }
         int input = scanner.nextInt();
@@ -357,6 +363,13 @@ public class VampireAdventureApp {
         }
     }
 
+    /**
+     * list all members of the party
+     * offset is used when the input of the menu is changing
+     * e.g. 0 is reserved for going back, offset would be 1
+     * 
+     * @param offset
+     */
     private static void listMembers(int offset) {
         Vampire[] members = playerParty.getMembers();
         int emptyCounter = 0;
@@ -365,10 +378,10 @@ public class VampireAdventureApp {
             if (members[i] == null) {
                 emptyCounter = emptyCounter + 1;
                 if (emptyCounter == members.length) {
-                    System.out.println("Looks like you don't have any members, maybe they all died!\n");
+                    System.out.println("Looks like you don't have any minions, maybe they all died!");
                 }
             } else {
-                System.out.println("(" + (i + offset) + ")\t" + members[i]);
+                System.out.println("(" + (i + offset) + ")\t" + members[i].getName());
             }
         }
     }
