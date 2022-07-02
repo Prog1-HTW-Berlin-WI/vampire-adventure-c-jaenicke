286c624896b1f5d056b91682cf7eddce846cd6f6
c-jaenicke <c-jaenicke@pm.me>
2022-07-02 13:56:09 +0200

    Major upate to class CreatorVampire

--- model/CreatorVampire.java
+++ model/CreatorVampire.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Random;
+
 /**
  * Class Creator Vampire
  * 
@@ -7,10 +9,61 @@ package model;
  * Extends the class Vampire
  */
 public class CreatorVampire extends Vampire {
-    // TOOD add attributets
 
-    // TODO add methods
-    public CreatorVampire(String name, int age){
-        super(name, age);
+    private Vampire descendants[] = new Vampire[10];
+
+    public CreatorVampire(String name, int age, Party playerParty) {
+        super(name, age, playerParty);
     }
+
+    public void listAllDescendants() {
+        // refresh list of descendants
+        descendants = playerParty.getMembers();
+
+        System.out.println("Your descendants are:");
+        for (int i = 0; i < playerParty.getMembers().length; i++) {
+            if (descendants[i] != null) {
+                System.out.println("\nName: " + descendants[i].getName());
+                System.out.println("\tAge: " + descendants[i].getAge());
+                System.out.println("\tGrandness: " + descendants[i].getGrandness());
+                System.out.println("\tHunger: " + descendants[i].getHunger());
+                System.out.println("\tEnergy: " + descendants[i].getEnergy());
+                System.out.println("\tPower: " + descendants[i].getPower());
+            }
+        }
+    }
+
+    public void commandToDrinkBlood(Vampire vampire, int amount, Human human) {
+        if (amount <= human.getAmountOfBlood()) {
+            vampire.drinkBlood(amount);
+            human.looseBlood(amount, vampire, playerParty);
+            System.err.println("The minion vampire drank " + amount + " liters of blood.");
+        } else {
+            vampire.drinkBlood(human.getAmountOfBlood());
+            human.looseBlood(human.getAmountOfBlood(), vampire, playerParty);
+            System.out.println("It seems, that the human has died.");
+        }
+    }
+
+    public void commandToAttack(Vampire vampire, Human human) {
+        vampire.attackHuman(human);
+    }
+
+    public void sacrifice(Vampire vampire, VampireHunter vampireHunter) {
+        // TODO
+    }
+
+    public boolean flee(Vampire vampire) {
+        Random flee = new Random();
+        int chance = flee.nextInt(101);
+
+        if (chance <= 40) {
+            System.out.println("You managed to escape the vampire hunter!");
+            return true;
+        } else {
+            System.out.println("You failed to escape the vampire hunter!");
+            return false;
+        }
+    }
+
 }
